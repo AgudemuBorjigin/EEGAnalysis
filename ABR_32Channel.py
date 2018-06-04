5971db641cb9c7b1c48188e0dba9ade372436aed
agudemuborjigan <agudemubor@gmail.com>
2018-06-04 10:23:56 -0400

    grand-average file was added

--- ABR_32Channel.py
+++ ABR_32Channel.py
@@ -1,15 +1,15 @@
 from anlffr.helper import biosemi2mne as bs
 import mne
-import numpy as np
-import os
-import fnmatch
+import numpy as np # support for large, multi-dimensional arrays and metrices 
 import pylab as pl
+import os ### 
+import fnmatch ###
 
 # Adding Files and locations
-froot = '/Users/baoagudemu1/Desktop/2018Spring/Lab'
+froot = '/Users/baoagudemu1/Desktop/2018Spring/Lab/EEG-Python'
 
-subjlist = ['S050',]
-### How were the conditions defined?
+subjlist = ['Hari',]
+# Those conditions are for trigger numbers
 conds = [[3, 9], [5, 10], [6, 12], [48, 144], [80, 160], [96, 192], [6], [12],
          [96], [192]]
 
@@ -21,7 +21,7 @@ for subj in subjlist:
     evelist = []
     
     # Extracting the bdf file names into bdfs
-    bdfs = fnmatch.filter(os.listdir(fpath), subj + '_ABR*.bdf')
+    bdfs = fnmatch.filter(os.listdir(fpath), subj + '_ITD*.bdf')
 
     if len(bdfs) >= 1:
         for k, bdf in enumerate(bdfs):
@@ -31,7 +31,7 @@ for subj in subjlist:
             extrachans = [u'GSR1', u'GSR2', u'Erg1', u'Erg2', u'Resp',
                           u'Plet', u'Temp']
             (rawtemp, evestemp) = bs.importbdf(edfname, nchans=36,
-                                               extrachans=extrachans)
+                                               extrachans=extrachans) ### rawtemp, evestemp
             rawtemp.set_channel_types({'EXG3': 'eeg', 'EXG4': 'eeg'})
             rawlist += [rawtemp, ]
             evelist += [evestemp, ]
@@ -39,14 +39,14 @@ for subj in subjlist:
         RuntimeError('No BDF files found!!')
     
     raw, eves = mne.concatenate_raws(rawlist, events_list=evelist)
-    # Filter the data
-    raw.filter(l_freq=30., h_freq=3000, picks=np.arange(36))
-    tmin, tmax = -0.01, 0.015
-    raw.info['bads'] += ['EXG3', 'EXG4', 'A1', 'A2', 'A30', 'A7', 'A6',
-                         'A24', 'A28', 'A29', 'A3', 'A11', 'A15',
-                         'A16', 'A17', 'A10', 'A21', 'A20', 'A25']
+    # Filter the data 
+    raw.filter(l_freq=1., h_freq=100, picks=np.arange(36))
+    tmin, tmax = -0.5, 2.5
+#    raw.info['bads'] += ['EXG3', 'EXG4', 'A1', 'A2', 'A30', 'A7', 'A6',
+#                         'A24', 'A28', 'A29', 'A3', 'A11', 'A15',
+#                         'A16', 'A17', 'A10', 'A21', 'A20', 'A25']
     # Channels on top of head are considered good
-    goods = [28, 3, 30, 26, 4, 25, 7, 31, 22, 9, 8, 21, 11, 12, 18]
+#    goods = [28, 3, 30, 26, 4, 25, 7, 31, 22, 9, 8, 21, 11, 12, 18]
     
     abrs = []
     pl.figure()
@@ -54,7 +54,7 @@ for subj in subjlist:
     for cond in conds:
         print 'Doing condition ', cond
         epochs = mne.Epochs(raw, eves, cond, tmin=tmin, proj=False,
-                            tmax=tmax, baseline=(-0.01, 0.0),
+                            tmax=tmax, baseline=(-0.5, 0.0),
                             picks=np.arange(36),
                             reject=dict(eeg=100e-6),
                             verbose='WARNING')
@@ -63,44 +63,43 @@ for subj in subjlist:
 # Putting all the averages of epochs into one file
 mne.write_evokeds(fpath + subj + '_ABR-ave.fif', abrs)
 
-# Plot data
-R = [3, 4, 5]
-L = [0, 1, 2]
-for k in L:
-    abr = abrs[k]
-    x = abr.data * 1e6  # microV
-    t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
-    ### Referenced to channel 34?
-    y = x[goods, :].mean(axis=0) - x[34, :]
-    ### Remiving DC offset?
-    y = y - y[(t > 0.) & (t < 1.)].mean()
-    pl.plot(t, y, linewidth=2)
-pl.xlabel('Time (ms)', fontsize=14)
-pl.ylabel('ABR (uV)', fontsize=14)
-pl.title('Left Ear', fontsize=14)
-pl.xlim((0., 10.))
-pl.ylim((-1.0, 1.5))
-ax = pl.gca()
-ax.tick_params(labelsize=14)
-pl.legend(['48 dB nHL', '64 dB nHL', '80 dB nHL'])
-pl.legend(['85 dB peSPL', '100 dB peSPL', '115 dB peSPL'])
-pl.show()
-
-pl.figure()
-for k in R:
-    abr = abrs[k]
-    x = abr.data * 1e6  # microV
-    t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
-    ### Referenced to channel 35?
-    y = x[goods, :].mean(axis=0) - x[35, :]
-    # y = y - y[(t > 0.5) & (t < 1.0)].mean()
-    pl.plot(t, y, linewidth=2)
-pl.xlabel('Time (ms)', fontsize=16)
-pl.ylabel('ABR (uV)', fontsize=16)
-pl.title('Right Ear', fontsize=20)
-pl.xlim((-1.0, 12.))
-pl.ylim((-1., 1.0))
-ax = pl.gca()
-ax.tick_params(labelsize=16)
-pl.legend(['85 dB peSPL', '100 dB peSPL', '115 dB peSPL'])
-pl.show()
+## Plot data
+#R = [3, 4, 5]
+#L = [0, 1, 2]
+#for k in L:
+#    abr = abrs[k]
+#    x = abr.data * 1e6  # microV
+#    t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
+#    ### Referenced to channel 34? mean(axis=0)
+#    y = x[goods, :].mean(axis=0) - x[34, :]
+#    y = y - y[(t > 0.) & (t < 1.)].mean()
+#    pl.plot(t, y, linewidth=2)
+#pl.xlabel('Time (ms)', fontsize=14)
+#pl.ylabel('ABR (uV)', fontsize=14)
+#pl.title('Left Ear', fontsize=14)
+#pl.xlim((0., 10.))
+#pl.ylim((-1.0, 1.5))
+#ax = pl.gca()
+#ax.tick_params(labelsize=14)
+#pl.legend(['48 dB nHL', '64 dB nHL', '80 dB nHL'])
+#pl.legend(['85 dB peSPL', '100 dB peSPL', '115 dB peSPL'])
+#pl.show()
+#
+#pl.figure()
+#for k in R:
+#    abr = abrs[k]
+#    x = abr.data * 1e6  # microV
+#    t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
+#    ### Referenced to channel 35?
+#    y = x[goods, :].mean(axis=0) - x[35, :]
+#    # y = y - y[(t > 0.5) & (t < 1.0)].mean()
+#    pl.plot(t, y, linewidth=2)
+#pl.xlabel('Time (ms)', fontsize=16)
+#pl.ylabel('ABR (uV)', fontsize=16)
+#pl.title('Right Ear', fontsize=20)
+#pl.xlim((-1.0, 12.))
+#pl.ylim((-1., 1.0))
+#ax = pl.gca()
+#ax.tick_params(labelsize=16)
+#pl.legend(['85 dB peSPL', '100 dB peSPL', '115 dB peSPL'])
+#pl.show()
---
+++ EEG_analysis.py
@@ -0,0 +1,106 @@
+import mne
+import numpy as np
+import os
+import fnmatch
+from anlffr.helper import biosemi2mne as bs
+from anlffr.preproc import find_blinks
+from mne.preprocessing.ssp import compute_proj_epochs
+from mne.time_frequency import tfr_multitaper
+
+# Adding Files and locations
+froot = 'D:/DATA/BindingEEG/'
+# froot = '/autofs/cluster/transcend/hari/ObjectFormation/'
+
+subjlist = ['SubjMS', ]
+para = 'Binding'
+epochs = []
+doTFR = False
+saveEpochs = False
+saveAve = False
+for subj in subjlist:
+
+    fpath = froot + subj + '/'
+
+    # These are so that the generated files are organized better
+    respath = fpath + 'RES/'
+
+    bdfs = fnmatch.filter(os.listdir(fpath), subj + '_' + para + '*.bdf')
+    print 'Viola!', len(bdfs),  'files found!'
+
+
+    # Load data and read event channel
+    rawlist = []
+    evelist = []
+    for k, rawname in enumerate(bdfs):
+        rawtemp, evestemp = bs.importbdf(fpath + rawname, verbose='DEBUG')
+        rawlist += [rawtemp, ]
+        evelist += [evestemp, ]
+    raw, eves = mne.concatenate_raws(rawlist, events_list=evelist)
+    
+    # raw.info['bads'] += []
+    # Filter the data for ERPs
+    raw.filter(l_freq=2.0, h_freq=40, l_trans_bandwidth=0.15)
+
+    # raw.apply_proj()
+    fs = raw.info['sfreq']
+    removeblinks = True
+
+    if removeblinks:
+        # SSP for blinks
+        blinks = find_blinks(raw, ch_name=['A1', ])
+        blinkname = (fpath + subj + '_' + para + '_blinks_erp' + '-eve.fif')
+        mne.write_events(blinkname, blinks)
+        epochs_blinks = mne.Epochs(raw, blinks, 998, tmin=-0.25,
+                                   tmax=0.25, proj=True,
+                                   baseline=(-0.25, 0),
+                                   reject=dict(eeg=500e-6))
+        blink_projs = compute_proj_epochs(epochs_blinks, n_grad=0,
+                                          n_mag=0, n_eeg=4,
+                                          verbose='DEBUG')
+        raw.add_proj(blink_projs)
+
+    evokeds = []
+    condnames = ['coh03', 'coh06', 'coh09', 'coh12']
+    condlists = [1, 2, 3, 4]
+    eves2 = np.zeros((eves.shape[0]*2, 3), dtype=np.int)
+    fs_int = int(raw.info['sfreq'])
+    for k, row in enumerate(eves):
+        eves2[2*k, :] = row + np.asarray([fs_int, 0, 0])
+        eves2[2*k + 1, :] = row + np.asarray([2*fs_int, 0, 0])
+
+    for k, condstem in enumerate(condnames):
+        condlist = condlists[k]
+        print 'Running Subject', subj, 'Condition', condstem
+
+        # Epoching events of type
+        epochs = mne.Epochs(raw, eves2, condlist, tmin=-0.4, proj=True,
+                            tmax=1.0, baseline=(-0.2, 0.0), name=condstem,
+                            reject=dict(eeg=100e-6))
+        evokeds += [epochs.average(), ]
+        fstem = subj + '_' + para
+        if doTFR:
+            freqs = np.arange(5., 70., 1.)
+            n_cycles = freqs * 0.2
+            power, itc = tfr_multitaper(epochs, freqs, n_cycles,
+                                        time_bandwidth=2.0, n_jobs=-1)
+            fname_pow = fstem + '_pow_' + condstem + '-tfr.h5'
+            fname_itc = fstem + '_itc_' + condstem + '-tfr.h5'
+            power.save(fpath + fname_pow)
+            itc.save(fpath + fname_itc)
+
+        if saveEpochs:
+            fname_epochs = fstem + '_' + condstem + '-epo.fif'
+            epochs.save(fpath + fname_epochs)
+
+    # Now save overall onset N100
+    epochs = mne.Epochs(raw, eves, condlists, tmin=-0.4, proj=True,
+                        tmax=1.0, baseline=(-0.2, 0.0), name='onset',
+                        reject=dict(eeg=100e-6))
+    evokeds += [epochs.average(), ]
+    if saveEpochs:
+            fname_epochs = fstem + '_onset-epo.fif'
+            epochs.save(fpath + fname_epochs)
+
+    if saveAve:
+        avename = subj + '_' + para + '_collapse-ave.fif'
+        mne.write_evokeds(fpath + avename, evokeds)
